3f3c3798d1d8bafac9106fe359ce86c245960592
leyendalerus <leyendalerus@hotmail.com>
2025-02-21 13:08:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  const who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  const action = ['se comió', 'orinó', 'aplastó', 'rompió'];
+  const what = ['mi tarea', 'mi teléfono', 'el coche'];
+  const when = ['antes de la clase', 'cuando estaba durmiendo', 'mientras estaba ejercitándome', 'durante mi almuerzo', 'mientras estaba rezando'];
 
+  const generateExcuse = () => {
+      const whoIndex = Math.floor(Math.random() * who.length);
+      const actionIndex = Math.floor(Math.random() * action.length);
+      const whatIndex = Math.floor(Math.random() * what.length);
+      const whenIndex = Math.floor(Math.random() * when.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+      return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+  };
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById('excuse').innerHTML = generateExcuse();
 };
